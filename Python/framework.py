73e3e4579fb860bbf74b8c17eb5e9068307112e5
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2024-09-30 12:21:20 +0100

    It is broken now but login

--- Python/framework.py
+++ Python/framework.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, redirect, render_template, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')                   
 # importing a flask object to create our app
 
@@ -22,7 +23,7 @@ class Role(db.Model):
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    name = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 class NameForm(FlaskForm):
@@ -34,14 +35,24 @@ with app.app_context():
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    comments = ['Card' , 'User']            
-    return render_template('index.html' , comments = comments, form = form, name = name)
+        user = User.query.filter_by(name=form.name.data).first()
+        if user is None:
+            user = User(name=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+            session['name'] = form.name.data
+            form.name.data = ''
+            return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), known = session.get('known', False))
+
 # routes the '/' page to our index page
+
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
