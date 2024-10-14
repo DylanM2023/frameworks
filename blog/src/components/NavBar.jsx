807639766f4bc6635f5ecb2352f2c3837b77d94d
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2024-10-14 15:34:55 +0100

    Add files via upload

---
+++ blog/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+// const NavBar = () => {
+    
+    
+    
+//     return ( <header className="bg-black text-white">
+//         <h1>{title}</h1>
+//     </header> );
+// }
+
+// export default NavBar;
---
+++ blog/src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useFetch = ((url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [isError, setIsError] = useState(null);
+    useEffect(() => {
+    
+        fetch(url).then((res) => {
+            if(!res.ok){
+                throw Error("Could not fetch data")
+            }
+        return res.json()
+        })
+        .then((data) => {
+            setTimeout(() => {
+            setData(data);
+            setIsPending(false)
+            }, 1000);
+        })
+        .catch((err) => {
+        setIsPending(false)
+        setIsError(err);
+        })
+    
+    }, [url]);
+
+    return {data, isPending, isError}
+})
+
+export default useFetch;
