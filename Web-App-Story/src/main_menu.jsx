68bec5ea92ded7efd96bd5587e3f3a6cecc47c93
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2024-10-22 15:21:18 +0100

    Stashed Some shii and added buttons to home screen

--- Web-App-Story/src/main_menu.jsx
+++ Web-App-Story/src/main_menu.jsx
@@ -1,11 +1,25 @@
 import NavBar from "./components/NavBar";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Menu = () => {
+
+    const navigate = useNavigate()
+    
+    const startGame = () => {
+        navigate("/option");
+    };
+
     return ( 
         <>
             <div className="bg-streetscene bg-no-repeat bg-cover bg-center h-screen w-screen text-white flex flex-col flex-wrap items-center"> 
                 <NavBar/>
-                <div className="bg-white w-8/12 grow font-typewriter text-black text-2xl flex flex-wrap"> 
+                <div className="w-8/12 grow font-typewriter text-black text-2xl flex flex-wrap"> 
+                <button className="bg-slate-300 h-1/8 w-full text-black rounded-xl p-1" onClick={startGame}>Start Game</button>
+                <button className="bg-slate-300 h-1/8 w-full text-black rounded-xl p-1">Settings</button>
+                <div className="bg-slate-300 h-1/8 w-full text-black flex flex-wrap flex-col items-center rounded-xl p-1">
+                    <a className="flex grow items-center p-1" href="https://github.com/DylanM2023/frameworks/" target="_blank">GitHub</a>
+                </div>
+                <button className="bg-slate-300 h-1/8 w-full text-black rounded-xl p-1">Exit</button>
                 </div>
             </div>
         </>
--- Web-App-Story/src/option.jsx
+++ Web-App-Story/src/option.jsx
@@ -7,15 +7,15 @@ return (
         <body>
             <div className="bg-streetscene bg-no-repeat bg-cover bg-center h-screen w-screen text-white flex flex-wrap justify-center">
             <NavBar/>
-            <div className="bg-white w-8/12 h-3/6 font-typewriter text-black text-2xl flex flex-wrap"> 
+            <div className="bg-white w-8/12 h-3/6 font-typewriter text-black text-2xl flex flex-wrap rounded-xl"> 
             <h1>21'st October 1943</h1>
-            <p className="p-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nihil optio inventore tenetur, eaque minima dolore rem quam similique molestias repudiandae, temporibus perferendis quae illum blanditiis eligendi ipsa necessitatibus soluta accusantium esse! Iusto fuga doloremque molestias nulla autem nihil, eum labore sit iure ea. Quo dolores a iste vero officia eos doloremque maxime temporibus voluptate ex omnis fuga sequi saepe sunt sed, cumque, quia at dolorem cum voluptatum sit assumenda labore mollitia accusamus! Mollitia sunt, laboriosam voluptatem quas dignissimos quis non distinctio error id unde cupiditate. Debitis repudiandae tempore neque expedita veritatis quo dolore error fuga illum maxime possimus voluptate, ipsum magnam vero delectus praesentium, temporibus rem sint. Deleniti voluptatum, voluptatem voluptatibus suscipit nisi quidem fugiat quas optio hic libero reprehenderit maxime officia voluptate, obcaecati dolor aliquid? Quod accusamus quo, illo officiis animi delectus dicta quam consectetur, quis atque deserunt iste non? Debitis corporis optio vero numquam, minima quasi quo?
+            <p className="p-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil fugiat fugit at animi, ea blanditiis. Consequatur amet, commodi nihil deserunt voluptatum a possimus placeat labore libero repellendus eligendi iusto error natus cupiditate velit odit provident veritatis nulla rem? Quibusdam dolores tempora optio ullam vel eveniet aut, mollitia ut assumenda praesentium eaque voluptates, accusantium id. Rem velit adipisci debitis veritatis ad eos, ex dolorem nemo impedit sint tenetur. Animi soluta consequatur, officia qui accusamus vel provident, excepturi ad accusantium rem recusandae dicta quod dignissimos at quam repellat eum. Quam ipsa commodi, quae totam odio inventore maxime mollitia est quibusdam aperiam quod?
             </p>
             </div>
             <div className="w-8/12 p-4 flex justify-center ">
-                <button className="w-2/6 bg-slate-400">Option 1</button>
+                <button className="w-2/6 bg-black text-white rounded-xl">Option 1</button>
                 <div className="w-2/6"></div>
-                <button className="w-2/6 bg-slate-400">Option 1</button>
+                <button className="w-2/6 bg-black text-white rounded-xl">Option 2</button>
             </div>
             </div>
         </body>
