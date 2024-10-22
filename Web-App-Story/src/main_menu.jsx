ae894bb28a35bd7c4a9706b51c403231d2341aa6
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2024-10-22 16:14:03 +0100

    Changed main menu

--- Web-App-Story/src/main_menu.jsx
+++ Web-App-Story/src/main_menu.jsx
@@ -12,14 +12,18 @@ const Menu = () => {
     return ( 
         <>
             <div className="bg-streetscene bg-no-repeat bg-cover bg-center h-screen w-screen text-white flex flex-col flex-wrap items-center"> 
-                <NavBar/>
-                <div className="w-8/12 grow font-typewriter text-black text-2xl flex flex-wrap"> 
-                <button className="bg-slate-300 h-1/8 w-full text-black rounded-xl p-1" onClick={startGame}>Start Game</button>
-                <button className="bg-slate-300 h-1/8 w-full text-black rounded-xl p-1">Settings</button>
-                <div className="bg-slate-300 h-1/8 w-full text-black flex flex-wrap flex-col items-center rounded-xl p-1">
-                    <a className="flex grow items-center p-1" href="https://github.com/DylanM2023/frameworks/" target="_blank">GitHub</a>
+                <div className="w-full h-1/6 bg-black flex flex-wrap flex-col items-center">
+                    <h1 className="flex grow items-center font-heading text-8xl p-2 bg-black text-cyan-300">
+                        Dylan's Web App Story
+                    </h1>
                 </div>
-                <button className="bg-slate-300 h-1/8 w-full text-black rounded-xl p-1">Exit</button>
+                <div className="w-8/12 grow font-typewriter text-black text-5xl flex flex-wrap"> 
+                    <div className="w-full h-6"></div>
+                    <button className="bg-slate-300 h-1/6 w-full text-black text-5xl rounded-xl p-10" onClick={startGame}>Start Game</button>
+                    <div className="bg-slate-300 h-1/6 w-full text-black text-5xl flex flex-wrap flex-col items-center rounded-xl p-1">
+                        <a className="flex grow items-center text-5xl p-1" href="https://github.com/DylanM2023/frameworks/" target="_blank">GitHub</a>
+                    </div>
+                    <button className="bg-slate-300 h-1/6 w-full text-black rounded-xl p-1">Exit</button>
                 </div>
             </div>
         </>
--- Web-App-Story/src/option.jsx
+++ Web-App-Story/src/option.jsx
@@ -8,7 +8,7 @@ return (
             <div className="bg-streetscene bg-no-repeat bg-cover bg-center h-screen w-screen text-white flex flex-wrap justify-center">
             <NavBar/>
             <div className="bg-white w-8/12 h-3/6 font-typewriter text-black text-2xl flex flex-wrap rounded-xl"> 
-            <h1>21'st October 1943</h1>
+            <h1 className="p-1" >21'st October 1943</h1>
             <p className="p-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil fugiat fugit at animi, ea blanditiis. Consequatur amet, commodi nihil deserunt voluptatum a possimus placeat labore libero repellendus eligendi iusto error natus cupiditate velit odit provident veritatis nulla rem? Quibusdam dolores tempora optio ullam vel eveniet aut, mollitia ut assumenda praesentium eaque voluptates, accusantium id. Rem velit adipisci debitis veritatis ad eos, ex dolorem nemo impedit sint tenetur. Animi soluta consequatur, officia qui accusamus vel provident, excepturi ad accusantium rem recusandae dicta quod dignissimos at quam repellat eum. Quam ipsa commodi, quae totam odio inventore maxime mollitia est quibusdam aperiam quod?
             </p>
             </div>
