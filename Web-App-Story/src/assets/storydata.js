e68c0d8763ed41f3e9e96ffdfb10050372ebb5de
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2024-11-04 12:47:55 +0000

    Added functionality
    Buttons work do not use "[]"

---
+++ Web-App-Story/src/assets/storydata.js
@@ -0,0 +1,25 @@
+const data = {
+    beginning: {
+        body_text: "November 4th 1947",
+        option_text_1: "Friend",
+        option_1_path: "friend_convo",
+        option_text_2: "Builder",
+        option_2_path:"builder_convo",
+    },
+    friend_convo: {
+        body_text: "Body number 2",
+        option_text_1: "No further Path",
+        option_1_path: null,
+        option_text_2: "No further Path",
+        option_2_path:null,
+    },
+    builder_convo: {
+        body_text: "Body number 3",
+        option_text_1: "No further Path",
+        option_1_path: null,
+        option_text_2: "No further Path",
+        option_2_path:null,
+    },
+}
+
+export default data;
