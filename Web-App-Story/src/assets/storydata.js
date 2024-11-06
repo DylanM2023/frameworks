c2d3780ecb5944905355eb46f3355f27681fcba2
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2024-11-06 12:41:12 +0000

    Added more to story

--- Web-App-Story/src/assets/storydata.js
+++ Web-App-Story/src/assets/storydata.js
@@ -4,7 +4,6 @@ const data = {
         body_text: ["VICTIM: Sharon Livingston, female approximately 5 ft 2 with long blonde hair and blue eyes. Wearing a large blue coat and a pearl necklace.",
                     "EVIDENCE: No autopsy performed yet, but from on the scene officers fatality seemed to be derived from a deep gash on her side, of which caused excessive bleeding. The bleeding being the cause of death is supported by the crimson new flair left on her blue coat.",
                     "NOTES: The Chief is in his usual lazy mood and doesn’t want to “Waste good officers on a case that lacks importance”. Due to our leader’s familiar incompetence this case has been passed around the office like a bad cold. I mean who would waste there time on a case if they only have 1 hour to solve it? So to whoever is reading this, I guess it's up to you whether you dismiss this case or... Dig a Little Bit Deeper.",
-                    ""
                     ],
         option_text_1: "Dismiss",
         option_1_path: null,
@@ -17,7 +16,7 @@ const data = {
         body_text: [
                     "WITNESSES: The first of the four witnesses is the victim's friend Nancy and the second witness is a business man named Mathew. Once again, you will only be able to speak with one of them so make sure you choose wisely.",
                     "THE FRIEND: Nancy was with Sharon when the murder took place and was the person who called us on to the scene. Nancy has no criminal record and appeared visibly distressed and unsettled. Any questioning from her will be more focused on getting more info than trying to get a confession.",
-                    "THE BUSINESS MAN: Mathew was taking the train home the night of the murder and was unaware of any attack or scene at the subway station. At least that's what he claims. When being brought in for questioning the man seemed reluctant, could be due to a busy schedule or something more sinister. The man also seemed unfazed when he was told about the murder leading us to believe he could be a good suspect."
+                    "THE BUSINESS MAN: Mathew was taking the train home the night of the murder and was unaware of any attack or scene at the subway station. At least that's what he claims. When being brought in for questioning the man seemed reluctant, could be due to a busy schedule or something more sinister."
                     ],
         option_text_1: "Talk to Mathew",
         option_1_path: "mathew_convo",
@@ -47,21 +46,47 @@ const data = {
                     ],
         option_text_1: "Did he seem dangerous?",
         option_1_path: "mathew_clear_2",
-        option_text_2: "Why did he seem odd?",
+        option_text_2: "Odd, how?",
         option_2_path: "mathew_teen_1",   
+    },
+
+    mathew_teen_1: {
+        title_text: "November 4th 1947, 02:19AM",
+        body_text: [
+                    "TRANSCRIPT: You ask:'Did you see anything suspicious?' Mathew responds:'Not really. I was super tired and barely made it to my stop. Although...', You say:'Go on...' Mathew:'Well there was this teenager who seemed... odd.'",
+                    "You ask:'Odd, how?', Mathew responds:'Well he was running away but he seemed more scared. Whether that is because he had commited a crime or witnessed it, I am not sure.' You respond:'Thank you Mathew, that will be all for now.'",
+                    "NOTES: Mathew seems a bit too eager to leave. This teenager seems like a possible suspect, I should probably investigate further. "
+                    ],
+        option_text_1: "Did he seem dangerous?",
+        option_1_path: "mathew_clear_2",
+        option_text_2: "Why did he seem odd?",
+        option_2_path: "mathew_teen_1",
     },
 
     mathew_clear_2: {
         title_text: "November 4th 1947, 02:19AM",
         body_text: [
-                    "TRANSCRIPT: Mathew asks:'How long is this is going to take?', You respond:'Depends on how quickly you answer my questions.'",
+                    "TRANSCRIPT: You ask:'Did you see anything suspicious?' Mathew responds:'Not really. I was super tired and barely made it to my stop. Although...', You say:'Go on...' Mathew:'Well there was this teenager who seemed... odd.'",
                     "You ask:'Did he seem dangerous?' Mathew responds:'Well... now that I think about it he was running away from the station. He seemed flustered like he was running from something.'",
                     "NOTES: Mathew seems a bit too eager to leave. This teenager seems like a possible suspect, I should probably investigate further. This teenager could've been fleeing a crime he commited."
                     ],
         option_text_1: "Did he appear armed?",
-        option_1_path: "mathew_clear_2",
+        option_1_path: "mathew_clear_3",
         option_text_2: "Do you think he did it?",
         option_2_path: "mathew_teen_1",   
+    },
+
+    mathew_clear_3: {
+        title_text: "November 4th 1947, 02:26AM",
+        body_text: [
+                    "TRANSCRIPT: You ask:'Did he seem dangerous?' Mathew responds:'Well... now that I think about it he was running away from the station. He seemed flustered like he was running from something.'",
+                    "You ask:'Did he appear armed?' Mathew responds:'I didn't see anything specific but he was definitely holding onto something in his hand.' You respond:'Thank you for your time Mathew.'",
+                    "NOTES: Mathew seems a bit too eager to leave. This teenager seems like a possible suspect, I should probably investigate further. This teenager could've been fleeing a crime he commited. Even a small blade could've caused the gash in Sharon's side."
+                    ],
+        option_text_1: "Leave Mathew",
+        option_1_path: null,
+        option_text_2: "Leave Mathew",
+        option_2_path: null,   
     },
 
     mathew_sus_1: {
